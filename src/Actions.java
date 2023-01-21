9e529ef4aa84fc5c5e7718287ae9d5f2a29b2b9c
ilya <ilia.lobow@gmail.com>
2023-01-21 19:27:48 +0500

    отображение рисунка test.jpg

--- src/Actions.java
+++ src/Actions.java
@@ -5,11 +5,14 @@ import java.awt.Graphics;
 
 public class Actions extends JPanel {
 
+    Image image = new ImageIcon("src/test.jpg").getImage();
+
     @Override
     public void paintComponent(Graphics graphics){
         super.paintComponent(graphics);
         graphics.setColor(Color.BLACK);
-        graphics.drawLine(200, 200, 1000, 1000);
+//        graphics.drawLine(200, 200, 1000, 1000); нарисовать линию
+        graphics.drawImage(image, 0, 0, 250,250,null);
     }
 
 }
--- src/Display.java
+++ src/Display.java
@@ -8,8 +8,8 @@ public class Display {
 
         JFrame frame = new JFrame(/* название нашей игры */); // создали рамку
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // выход из программы
-        frame.setSize(700,700);
-        frame.setResizable(false);
+        frame.setSize(1200,800);
+        frame.setResizable(true);
         frame.setUndecorated(true);
         frame.add(new Actions());
         frame.setVisible(true); // рамка видимая
