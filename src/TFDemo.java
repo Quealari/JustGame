fdd5e2a7a97dff88b57dd3a0160680dc49d4ae05
ilya <ilia.lobow@gmail.com>
2023-01-25 17:15:59 +0500

    // TFDemo отладка: добавление поля ввода и обработчика событий

--- src/TFDemo.java
+++ src/TFDemo.java
@@ -6,31 +6,56 @@ import java.awt.event.ActionListener;
 public class TFDemo implements ActionListener {
 
 
-    public TextField textField;
-    public JLabel label;
+    public JTextField textField;
+    public JLabel label, labelPrompt, labelContents;
+    JButton buttonRevers;
 
 
     public TFDemo() {
 
-        Frame frame = new JFrame("Test TFDemo");
-        frame.setSize(new Dimension(2550, 1500));
 
+        // создать новый контейнер JFrame
+        JFrame frame = new JFrame("Test TFDemo");
+
+        // установить диспетчер компоновки FlowLayout
         frame.setLayout(new FlowLayout());
 
-        JButton buttonOne = new JButton();
-        JButton buttonTwo = new JButton();
-        buttonOne = new JButton("Первая кнопка");
-        buttonTwo = new JButton("Вторая кнопка");
-        frame.add(buttonOne);
-        frame.add(buttonTwo);
+        // задать исходные размеры рамки окна
+        frame.setSize(new Dimension(350, 250));
 
+        // завершить программу после закрытия окна
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //создание поля ввода шириной 10 символов
+        textField = new JTextField(10);
+
+        // создать кнопку Reverse
+        buttonRevers = new JButton("Reverse");
+
+        JButton buttonOne = new JButton("Первая кнопка");
+        JButton buttonTwo = new JButton("Вторая кнопка");
+
+        // Добавление приемников событий от поля ввода и кнопки.
         buttonOne.addActionListener(this);
         buttonTwo.addActionListener(this);
+        buttonRevers.addActionListener(this);
+        textField.addActionListener(this);
 
+        // создать метки
         label = new JLabel("Нажмите кнопку");
+        labelPrompt = new JLabel("Enter text: "); //
+        labelContents = new JLabel(""); //
+
+        // добавить компоненты на панель содержимого
+        frame.add(labelPrompt);
+        frame.add(textField);
+        frame.add(labelContents);
         frame.add(label);
+        frame.add(buttonOne);
+        frame.add(buttonTwo);
+        frame.add(buttonRevers);
 
-
+        //отобразирть рамку окна
         frame.setVisible(true);
 
 
@@ -44,8 +69,20 @@ public class TFDemo implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         if (actionEvent.getActionCommand().equals("Первая кнопка")) {
-            label.setText("Вы нажали неправильную кнопку");
-        } else label.setText("Вы нажали правильную кнопку");
+            label.setText("Вы нажали первую кнопку");
+        } else if (actionEvent.getActionCommand().equals("Вторая кнопка")) {
+            label.setText("Вы нажали вторую кнопку");
+        } else if (actionEvent.getActionCommand().equals("Reverse")) {
+            String orgStr = textField.getText();
+            String cleanStr = " ";
 
+            // обратить символьную строку в поле ввода текста
+            for (int i = orgStr.length() - 1; i >= 0; i--) {
+                cleanStr = cleanStr + orgStr.charAt(i);
+                textField.setText(cleanStr);
+            }
+        }
+        else labelContents.setText("You pressed ENTER. Text is: " +
+                    textField.getText());
     }
 }
